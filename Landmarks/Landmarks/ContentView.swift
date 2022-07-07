a3f7d8e48346814b7390d8065651a784ab6c5a5c
didwndckd <didwndckd@gmail.com>
2022-07-07 14:12:04 +0900

    add: Landmarks 프로젝트 추가

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 양중창 on 2022/07/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
