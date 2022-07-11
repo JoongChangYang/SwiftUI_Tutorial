bdfa900c59563c503266169b2e014bd075e87028
didwndckd <didwndckd@gmail.com>
2022-07-11 15:26:36 +0900

    add: BuildingListAndNavigation

---
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/BuildingListsAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingListsAndNavigationApp.swift
+//  BuildingListsAndNavigation
+//
+//  Created by 양중창 on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct BuildingListsAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 
 
 - [Landmarks](Landmarks)
+- [BuildingListsAndNavigation](BuildingListsAndNavigation)
 
