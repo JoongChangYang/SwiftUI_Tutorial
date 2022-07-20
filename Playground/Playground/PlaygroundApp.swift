d2447b264cb58521051201f8a195405c88003057
didwndckd <didwndckd@gmail.com>
2022-07-20 17:44:27 +0900

    add: Playground

---
+++ Playground/Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by 양중창 on 2022/07/20.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
