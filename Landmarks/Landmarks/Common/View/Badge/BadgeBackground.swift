667eea7e36af1d48f7e2abd2c4004f1cff01cc53
didwndckd <didwndckd@gmail.com>
2022-09-18 00:51:18 +0900

    test: Path 테스트

--- Landmarks/Landmarks/Common/View/Badge/BadgeBackground.swift
+++ Landmarks/Landmarks/Common/View/Badge/BadgeBackground.swift
@@ -12,33 +12,48 @@ struct BadgeBackground: View {
     private let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
     
     var body: some View {
-        GeometryReader { geometry in
-            Path { path in
-                var width: CGFloat = min(geometry.size.width, geometry.size.height)
-                let height = width
-                let xScale: CGFloat = 0.832
-                let xOffset = (width * (1.0 - xScale)) / 2.0
-                width *= xScale
-                
-                path.move(to: CGPoint(x: width * 0.95 + xOffset,
-                                      y: height * (0.20 + HexagonParameters.adjustment)))
-                
-                HexagonParameters.segments.forEach { segment in
+        VStack() {
+            GeometryReader { geometry in
+                Path { path in
+                    var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                    let height = width
+                    let xScale: CGFloat = 0.832
+                    let xOffset = (width * (1.0 - xScale)) / 2.0
+                    width *= xScale
                     
-                    path.addLine(to: CGPoint(x: width * segment.line.x + xOffset,
-                                             y: height * segment.line.y))
+                    path.move(to: CGPoint(x: width * 0.95 + xOffset,
+                                          y: height * (0.20 + HexagonParameters.adjustment)))
                     
-                    path.addQuadCurve(to: CGPoint(x: width * segment.curve.x + xOffset,
-                                                  y: height * segment.curve.y),
-                                      control: CGPoint(x: width * segment.control.x + xOffset,
-                                                       y: height * segment.control.y))
+                    HexagonParameters.segments.forEach { segment in
+                        
+                        path.addLine(to: CGPoint(x: width * segment.line.x + xOffset,
+                                                 y: height * segment.line.y))
+                        
+                        path.addQuadCurve(to: CGPoint(x: width * segment.curve.x + xOffset,
+                                                      y: height * segment.curve.y),
+                                          control: CGPoint(x: width * segment.control.x + xOffset,
+                                                           y: height * segment.control.y))
+                    }
+                }
+                .fill(.linearGradient(Gradient(colors: [self.gradientStart, self.gradientEnd]),
+                                      startPoint: UnitPoint(x: 0.5, y: 0),
+                                      endPoint: UnitPoint(x: 0.5, y: 0.6)))
+            }
+            .aspectRatio(1, contentMode: .fit)
+            
+            GeometryReader { proxy in
+                Path { path in
+                    var width = proxy.size.width
+                    let height = proxy.size.height
+                    let center = proxy.size.width / 2
+                    path.move(to: CGPoint(x: center, y: 0))
+                    path.addLine(to: CGPoint(x: width, y: height))
+                    path.addLine(to: CGPoint(x: 0, y: height))
                 }
             }
-            .fill(.linearGradient(Gradient(colors: [self.gradientStart, self.gradientEnd]),
-                                  startPoint: UnitPoint(x: 0.5, y: 0),
-                                  endPoint: UnitPoint(x: 0.5, y: 0.6)))
+            .background(Color.pink)
         }
-        .aspectRatio(1, contentMode: .fit)
+        
     }
 }
 
