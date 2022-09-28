0513e53263475bf21eaf49c3f3e4bc7b6cc9231f
didwndckd <didwndckd@gmail.com>
2022-09-29 01:25:27 +0900

    feat: 섹션 2 완료

--- Landmarks/Landmarks/Common/View/PageViewController.swift
+++ Landmarks/Landmarks/Common/View/PageViewController.swift
@@ -11,17 +11,54 @@ import UIKit
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(transitionStyle: .scroll,
                                                       navigationOrientation: .horizontal,
                                                       options: nil)
+        pageViewController.dataSource = context.coordinator
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
-        pageViewController.setViewControllers([UIHostingController(rootView: self.pages[0])],
+        pageViewController.setViewControllers([context.coordinator.controllers[0]],
                                               direction: .forward,
                                               animated: true)
+    }
+}
+
+extension PageViewController {
+    final class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+
+        init(_ pageViewController: PageViewController) {
+            self.parent = pageViewController
+            self.controllers = self.parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = self.controllers.firstIndex(of: viewController) else { return nil }
+            
+            if index == 0 {
+                return self.controllers.last
+            }
+            
+            return self.controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = self.controllers.firstIndex(of: viewController) else { return nil }
+            
+            if index + 1 == self.controllers.count {
+                return self.controllers.first
+            }
+            
+            return self.controllers[index + 1]
+        }
     }
 }
